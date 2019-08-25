e6e46e7688a2bddfc6d012e0035895a71d1bc1d3
Danya0x07 <danya9104449383@gmail.com>
2019-08-25 17:12:32 +0300

    Добавление элегантности.

--- src/radio.c
+++ src/radio.c
@@ -7,10 +7,29 @@
 #include "delay.h"
 #include "debug.h"
 
-
 void radio_init(void)
 {
     static uint8_t address[4] = {0xC7, 0x68, 0xAC, 0x35};
+    /* Включаем проверку контрольной суммы, выключаем прерывания на ноге IRQ
+       и переводимся в режим передатчика. */
+    const uint8_t config = PWR_UP | EN_CRC | MASK_TX_DS | MASK_RX_DR | MASK_MAX_RT;
+    /* Выбираем частотный канал. */
+    const uint8_t rf_ch = 33;  
+    /* Выбираем скорость работы 1 Мбит/с и мощность 0 dbm. */
+    const uint8_t rf_setup = 0 | RF_SETUP_0DBM;
+    /* Выбираем длину адреса в 4 байта. */
+    const uint8_t setup_aw = SETUP_AW_4BYTES_ADDRESS;
+    /* Включаем канал 0 для приёма подтверждений. */
+    const uint8_t en_rxaddr = ERX_P0;
+    const uint8_t en_aa = ENAA_P0;
+    /* Включаем возможность не указывать размер полезной нагрузки
+       и отправлять пакеты, не требующие подтверждения. */
+    const uint8_t feature = EN_DPL | EN_DYN_ACK | EN_ACK_PAY;
+    /* Включаем динамическую длину полезной нагрузки на канале 0. */
+    const uint8_t dynpd = DPL_P0;
+    /* Настраиваем повторную отправку. */
+    const uint8_t setup_retr = (SETUP_RETR, SETUP_RETR_DELAY_750MKS
+                                | SETUP_RETR_UP_TO_5_RETRANSMIT);
     /* Подали питание. */
     nrf_init();
     delay_ms(100);
@@ -19,67 +38,52 @@ void radio_init(void)
     delay_ms(5);
 
     /* TODO: Попробовать что-нибудь убрать, чтобы не сломалось. */
-    
-    /* Включаем проверку контрольной суммы, выключаем прерывания на ноге IRQ
-       и переводимся в режим передатчика. */
-    nrf_rmw_byte(CONFIG, EN_CRC | MASK_TX_DS | MASK_RX_DR | MASK_MAX_RT, SET);
 
-    /* Выбираем частотный канал. */
-    nrf_overwrite_byte(RF_CH, 33);
-    /* Выбираем скорость работы 1 Мбит/с и мощность 0 dbm. */
-    nrf_overwrite_byte(RF_SETUP, 0 | RF_SETUP_0DBM);
-    
-    /* Выбираем длину адреса в 4 байта. */
-    nrf_overwrite_byte(SETUP_AW, SETUP_AW_4BYTES_ADDRESS);
-    /* Настраиваем адрес канала. */
-    nrf_rw_buff(W_REGISTER | TX_ADDR, address, 4, NRF_OPERATION_WRITE);
+    /* Записываем настройки в модуль. */
+    nrf_rmw_byte(CONFIG, config, SET);
+    nrf_overwrite_byte(RF_CH,      rf_ch);
+    nrf_overwrite_byte(RF_SETUP,   rf_setup);
+    nrf_overwrite_byte(SETUP_AW,   setup_aw);
+    nrf_overwrite_byte(EN_RXADDR,  en_rxaddr);
+    nrf_overwrite_byte(EN_AA,      en_aa);
+    nrf_overwrite_byte(FEATURE,    feature);
+    nrf_overwrite_byte(DYNPD,      dynpd);
+    nrf_overwrite_byte(SETUP_RETR, setup_retr);
+    /* Записываем адрес канала. */
+    nrf_rw_buff(W_REGISTER | TX_ADDR,    address, 4, NRF_OPERATION_WRITE);
     nrf_rw_buff(W_REGISTER | RX_ADDR_P0, address, 4, NRF_OPERATION_WRITE);
 
-    /* Включаем возможность не указывать размер полезной нагрузки
-       и отправлять пакеты, не требующие подтверждения. */
-    nrf_overwrite_byte(FEATURE, EN_DPL | EN_DYN_ACK | EN_ACK_PAY);
-    
-    /* Включаем канал 0 для приёма подтверждений. */
-    nrf_overwrite_byte(EN_RXADDR, ERX_P0);
-    nrf_overwrite_byte(EN_AA, ENAA_P0);
-    /* Включаем динамическую длину полезной нагрузки на канале 0. */
-    nrf_overwrite_byte(DYNPD, DPL_P0);
-    
-    /* Настраиваем повторную отправку. */
-    nrf_overwrite_byte(SETUP_RETR, SETUP_RETR_DELAY_750MKS
-                       | SETUP_RETR_UP_TO_5_RETRANSMIT);
-
-    /* TODO: Сделать покороче. */
-#if 1
-    if (nrf_read_byte(CONFIG) != (PWR_UP | MASK_RX_DR | MASK_TX_DS | MASK_MAX_RT | EN_CRC)) {
-        logs("config failed\n");
+    /* Проверяем, что модуль всё понял. */
+    if (nrf_read_byte(CONFIG) != config) {
+        logs("config\n");
     }
-    if (nrf_read_byte(RF_CH) != 33) {
-        logs("channel failed\n");
+    if (nrf_read_byte(RF_CH) != rf_ch) {
+        logs("rf_ch\n");
     }
-    if (nrf_read_byte(RF_SETUP) != RF_SETUP_0DBM) {
+    if (nrf_read_byte(RF_SETUP) != rf_setup) {
         logs("rf_setup failed\n");
     }
-    if (nrf_read_byte(SETUP_AW) != SETUP_AW_4BYTES_ADDRESS) {
+    if (nrf_read_byte(SETUP_AW) != setup_aw) {
         logs("setup_aw failed\n");
     }
-    if (nrf_read_byte(FEATURE) != (EN_DPL | EN_DYN_ACK | EN_ACK_PAY)) {
-        logs("feature failed\n");
-    }
-    if (nrf_read_byte(EN_RXADDR) != ERX_P0) {
+    if (nrf_read_byte(EN_RXADDR) != en_rxaddr) {
         logs("en_rxaddr failed\n");
     }
-    if (nrf_read_byte(EN_AA) != ENAA_P0) {
+    if (nrf_read_byte(EN_AA) != en_aa) {
         logs("enaa failed\n");
     }
-    if (nrf_read_byte(DYNPD) != DPL_P0) {
+    if (nrf_read_byte(FEATURE) != feature) {
+        logs("feature failed\n");
+    }
+    if (nrf_read_byte(DYNPD) != dynpd) {
         logs("dynpd failed\n");
     }
-    if (nrf_read_byte(SETUP_RETR) != (SETUP_RETR_DELAY_750MKS | SETUP_RETR_UP_TO_5_RETRANSMIT)) {
-        logs("retr failed\n");
+    if (nrf_read_byte(SETUP_RETR) != setup_retr) {
+        logs("setup_retr\n");
     }
-#endif
-
+    /* Адреса проверять не буду, потому что если ничего из вышеперечисленного
+       не сломалось, значит и они вряд ли сломаются (ага, вряд ли:). */
+    
     /* Чистим буферы на всякий случай. */
     nrf_cmd(FLUSH_TX);
     nrf_cmd(FLUSH_RX);
