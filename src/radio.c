d057e41c12d673da3a9ad29052ed935250e603ae
Danya0x07 <danya9104449383@gmail.com>
2019-09-14 21:04:51 +0300

    попытка убрать конфликт слияния

--- src/radio.c
+++ src/radio.c
@@ -14,7 +14,7 @@ void radio_init(void)
        и переводимся в режим передатчика. */
     const uint8_t config = PWR_UP | EN_CRC | MASK_TX_DS | MASK_RX_DR | MASK_MAX_RT;
     /* Выбираем частотный канал. */
-    const uint8_t rf_ch = 33;  
+    const uint8_t rf_ch = 112;  
     /* Выбираем скорость работы 1 Мбит/с и мощность 0 dbm. */
     const uint8_t rf_setup = 0 | RF_SETUP_0DBM;
     /* Выбираем длину адреса в 4 байта. */
@@ -49,7 +49,7 @@ void radio_init(void)
     /* Если с модулем что-то не в порядке, то ни один регистр не запишется,
        поэтому одной проверки должно быть достаточно. */
     if (nrf_read_byte(CONFIG) != config) {
-        logs("nrf not responding!\n");
+        led_blink(3, 300);
     }
     
     /* Чистим буферы на всякий случай. */
@@ -60,44 +60,43 @@ void radio_init(void)
     TIM3_Cmd(ENABLE);
 }
 
-void radio_send_data(DataToRobot* data_to_robot)
+bool radio_send_data(DataToRobot* data_to_robot)
 {
+    bool conn_err_occured = FALSE;
     uint8_t status = nrf_get_status();
     if (status & TX_FULL_STATUS)  {
         nrf_cmd(FLUSH_TX);
-        logs("tx full\n");
     }
     if (status & MAX_RT) {
         nrf_cmd(FLUSH_TX);
         nrf_clear_interrupts();
-        logs("max_rt\n");
+        conn_err_occured = TRUE;
     }
-    logi(data_to_robot->direction); logs("\t");
-    logi(data_to_robot->speed_left); logs("\t");
-    logi(data_to_robot->speed_right); logs("\t");
-    logi(data_to_robot->control_flags); logs("\n");
     nrf_rw_buff(W_TX_PAYLOAD, (uint8_t*) data_to_robot,
                 sizeof(DataToRobot), NRF_OPERATION_WRITE);
     nrf_ce_1();
     delay_ms(1);
     nrf_ce_0();
+    return conn_err_occured;
 }
 
 void radio_check_for_incoming(DataFromRobot* data_from_robot)
 {
-    uint8_t fifo_status = nrf_read_byte(FIFO_STATUS);
-    if (!(fifo_status & RX_EMPTY)) {
-        uint8_t data_size;
-        nrf_rw_buff(R_RX_PL_WID, &data_size, 1, NRF_OPERATION_READ);
-        if (data_size != sizeof(DataFromRobot)) {
-            nrf_cmd(FLUSH_RX);
-            return;
-        }
-        nrf_rw_buff(R_RX_PAYLOAD, (uint8_t*) data_from_robot,
-                    sizeof(DataFromRobot), NRF_OPERATION_READ);
-        logi(data_from_robot->battery_brains); logs("\t");
-        logi(data_from_robot->battery_motors); logs("\t");
-        logi(data_from_robot->temperature);    logs("\t\n");
+    uint8_t status = nrf_get_status();
+    if (status & RX_DR) {
+        uint8_t fifo_status, data_size;
+        do {
+            nrf_rw_buff(R_RX_PL_WID, &data_size, 1, NRF_OPERATION_READ);
+            if (data_size != sizeof(DataFromRobot)) {
+                nrf_cmd(FLUSH_RX);
+                nrf_clear_interrupts();
+                return;
+            }
+            nrf_rw_buff(R_RX_PAYLOAD, (uint8_t*) data_from_robot,
+                        sizeof(DataFromRobot), NRF_OPERATION_READ);
+            nrf_clear_interrupts();
+            fifo_status = nrf_read_byte(FIFO_STATUS);
+        } while (!(fifo_status & RX_EMPTY));
     }
 }
 
@@ -114,16 +113,16 @@ bool radio_data_to_robot_is_new(const DataToRobot* data_to_robot)
     static DataToRobot buffer_to_robot;
     
     bool data_is_new = (
-        buffer_to_robot.direction     != data_to_robot->direction   ||
-        buffer_to_robot.speed_left    != data_to_robot->speed_left  ||
-        buffer_to_robot.speed_right   != data_to_robot->speed_right ||
-        buffer_to_robot.control_flags != data_to_robot->control_flags
+        buffer_to_robot.direction   != data_to_robot->direction   ||
+        buffer_to_robot.speed_left  != data_to_robot->speed_left  ||
+        buffer_to_robot.speed_right != data_to_robot->speed_right ||
+        buffer_to_robot.control_reg != data_to_robot->control_reg
     );
     if (data_is_new) {
-        buffer_to_robot.direction     = data_to_robot->direction;
-        buffer_to_robot.speed_left    = data_to_robot->speed_left;
-        buffer_to_robot.speed_right   = data_to_robot->speed_right;
-        buffer_to_robot.control_flags = data_to_robot->control_flags;
+        buffer_to_robot.direction   = data_to_robot->direction;
+        buffer_to_robot.speed_left  = data_to_robot->speed_left;
+        buffer_to_robot.speed_right = data_to_robot->speed_right;
+        buffer_to_robot.control_reg = data_to_robot->control_reg;
     }
     return data_is_new;
 }
